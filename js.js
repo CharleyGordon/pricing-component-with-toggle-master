9072cab5872cb02ab7da38aea6cc7bf14fe4c14d
CharleyGordon <nicksawage@gmail.com>
2022-10-08 10:58:23 +0200

    Update js.js

--- js.js
+++ js.js
@@ -6,18 +6,20 @@ const master = document.querySelector('[class*="master"]');
 let basicPrice = ["199.99", "19.99"];
 let professionalPrice = ["249.99", "24.99"];
 let masterPrice = ["399.99", "39.99"];
-
-checkbox.addEventListener("click", () => {
+let changeText = () => {
   switch (checkbox.checked) {
     case false:
       basic.innerText = basic.innerText[0] + basicPrice[0];
       professional.innerText = professional.innerText[0] + professionalPrice[0];
       master.innerText = master.innerText[0] + masterPrice[0];
       break;
+
     case true:
       basic.innerText = basic.innerText[0] + basicPrice[1];
       professional.innerText = professional.innerText[0] + professionalPrice[1];
       master.innerText = master.innerText[0] + masterPrice[1];
       break;
   }
-});
+};
+window.addEventListener("load", changeText);
+checkbox.addEventListener("click", changeText);
